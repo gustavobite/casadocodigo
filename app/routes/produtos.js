650964a62b9abc63ebdc0a9adec4a64f0c37630e
Gustavo Bitencourt <gustavobite@gmail.com>
2018-05-15 22:48:33 -0300

    Added mysql driver and database configuration

--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -1,5 +1,12 @@
 module.exports = function(app) {
-    app.get('/produtos', function(require, response) {
-        response.render("produtos/lista");
+    app.get("/produtos",function(req, res) {
+        var connection = app.infra.connectionFactory();
+
+        connection.query('select * from produtos', function(err, results){
+            res.render('produtos/lista', {lista: results});
+        });
+
+        connection.end();
+
     });
 }
